511165ee87e614d9639e7e1dd527fb92e05e30e1
KanaeToono <lethitrang2910@gmail.com>
2016-08-08 14:21:44 +0700

    blog radio source code

---
+++ app/src/main/java/oliverdev/com/blogradio/MainActivity.java
@@ -0,0 +1,13 @@
+package oliverdev.com.blogradio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
