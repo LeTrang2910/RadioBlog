6b71af6273863e6fbba730beb8be449eee885f67
KanaeToono <lethitrang2910@gmail.com>
2016-08-10 16:23:12 +0700

    put MainActivity into activites package

---
+++ app/src/main/java/oliverdev/com/blogradio/activities/MainActivity.java
@@ -0,0 +1,14 @@
+package oliverdev.com.blogradio.activities;
+
+import org.androidannotations.annotations.EActivity;
+
+import oliverdev.com.blogradio.R;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void initViews() {
+
+    }
+}
